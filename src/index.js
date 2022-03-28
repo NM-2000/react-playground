6522d34a88855908503b2d2ee15f5587a11bcd27
NM-2000 <nikhilmittal3434@gmail.com>
2022-03-29 01:57:41 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './hello_world';
+import Timer from './timer';
 
 /**
  * render the React component (along with its props) passed as the 1st argument,
  * inside the element specified using 2nd argument.
  */
-ReactDOM.render(<HelloWorld name="John Doe" />, document.getElementById("root"));
+
+// ReactDOM.render(<HelloWorld name="John Doe" />, document.getElementById("root"));
+
+
+
+/**
+ * render the Timer component.
+ */
+
+ReactDOM.render(<Timer />, document.getElementById("root"));
+
+

7fd2cc7a5bf7849b40e53e9b9d470f95f53f36a9
NM-2000 <nikhilmittal3434@gmail.com>
2022-03-31 00:57:52 +0530

    Updated files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './hello_world';
 import Timer from './timer';
+import Todo from './todo_list';
 
 /**
  * render the React component (along with its props) passed as the 1st argument,
@@ -16,6 +17,11 @@ import Timer from './timer';
  * render the Timer component.
  */
 
-ReactDOM.render(<Timer />, document.getElementById("root"));
+// ReactDOM.render(<Timer />, document.getElementById("root"));
 
 
+
+/**
+ * render the Todo-list component.
+ */
+ReactDOM.render(<Todo />, document.getElementById("root"));
